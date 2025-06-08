df74e1e09c2fc38b7961b3e36e5682709023abdf
Elizabeth Orji <cegscutie4rill@yahoo.com>
2025-06-08 16:15:12 +0100

    creted charcount file

---
+++ CharacterCount.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class CharacterCount {
+    public static class CharMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text character = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            for (char c : value.toString().toCharArray()) {
+                character.set(String.valueOf(c));
+                context.write(character, one);
+            }
+        }
+    }
+
+    public static class CharReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) sum += val.get();
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "character count");
+        job.setJarByClass(CharacterCount.class);
+        job.setMapperClass(CharMapper.class);
+        job.setCombinerClass(CharReducer.class);
+        job.setReducerClass(CharReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
